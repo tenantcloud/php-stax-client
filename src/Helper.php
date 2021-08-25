c4bf87da2cf8f46af1ce0ef297d4ad28975b21f3
tandre <a.tershak@tenantcloud.com>
2021-08-25 17:59:37 +0300

    fix: Fixed code style

--- src/Helper.php
+++ src/Helper.php
@@ -7,8 +7,8 @@ class Helper extends ApiResource
 	public function setupGatewayForMerchant(string $id): void
 	{
 		$this->request('post', "merchant/{$id}/gateway/", [
-			'name' => 'test',
-			'type' => 'TEST',
+			'name'   => 'test',
+			'type'   => 'TEST',
 			'vendor' => 'spreedly',
 		]);
 	}
