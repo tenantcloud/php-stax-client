6439be838c8a326cf1e6c80236db507d4967de0f
tandre <a.tershak@tenantcloud.com>
2021-08-25 17:59:04 +0300

    misc: Added helper

---
+++ src/Helper.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Stax;
+
+class Helper extends ApiResource
+{
+	public function setupGatewayForMerchant(string $id): void
+	{
+		$this->request('post', "merchant/{$id}/gateway/", [
+			'name' => 'test',
+			'type' => 'TEST',
+			'vendor' => 'spreedly',
+		]);
+	}
+}
